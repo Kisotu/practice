ac05aba05a2cf1a43c594d628a0adac84d6930ae
Oloishorua <jacoboloishorua@gmail.com>
2023-08-28 23:58:46 +0300

    **constructor methods**

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const pchange = document.getElementById('status');
+function Book(title, author, year){
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+    this.getSummary = function(){
+        return `${this.title} was written by ${this.author} in the year ${this.year}`
+    }
+}
+
+const book1 = new Book("Book 1", "James Prausw", 2013);
+const book2 = new Book("Book 2", "Machina Exter", 2023)
+
+console.log(book1.getSummary())
+console.log(book2.getSummary())
